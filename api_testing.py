60a2f107596074b438ba5fc2807ec2c7dc5557bd
Anshul Sanghi <xzaviourr@gmail.com>
2022-04-24 13:08:30 +0530

    kg algo working

--- api_testing.py
+++ api_testing.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify, request
+import time
 
 api = Flask(__name__)
+
+@api.route('/api', methods = ['GET'])
+def returnascii():
+    d = {}
+    inputchr = str(request.args['query'])
+    print("API HIT")
+    time.sleep(2)
+    d['output'] = "Hii! I've received your message"
+    return d
 
 @api.route('/', methods = ['GET'])
 def show_homepage():
